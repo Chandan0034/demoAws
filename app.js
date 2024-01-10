abb3a9e2c5d2b4f20ac8bca8211cd00d3bde9c65
Chandan kumar <chandandpu@gmail.com>
2024-01-10 20:43:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express=require('express');
+const cors=require('cors')
+const {exec}=require('child_process')
+const fs=require('fs')
+const app=express();
+app.use(cors());
+app.use(express.json());
+require('dotenv').config();
+const PORT=process.env.PORT||8000;
+app.get('/',async(req,res)=>{
+    res.send("Welcome To Online Code Runner Application")
+})
+app.post('/code',async(req,res)=>{
+    const id=req.headers;
+    const {code,language}=req.body;
+    const FileName=`Main.${language}`
+    fs.writeFileSync(FileName,code);
+    const child_process=exec(`g++ Main.cpp -o Main && ./Main`,(error,stdout,stderr)=>{
+        if(error){
+            res.send({
+                id:id,
+                output:stderr,
+                success:false,
+                message:"Execution Error"
+            })
+        }
+    })
+    child_process.stdout.on('data',(data)=>{
+        res.send({
+            id:id,
+            output:data,
+            success:True,
+            message:"Execution Successfully"
+        })
+    })
+
+})
+app.listen(PORT,()=>{
+    console.log(`Server Started At Port ${PORT}`);
+})
